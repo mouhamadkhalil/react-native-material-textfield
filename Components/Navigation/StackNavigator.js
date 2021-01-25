0e35fd8588f823f95f81d846b05a3896acbd7ed0
nassirGH <nassir.ghraizi8@gmail.com>
2021-01-25 12:10:36 +0200

    Done

--- Components/Navigation/StackNavigator.js
+++ Components/Navigation/StackNavigator.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
+
 import Login from "../Start/LoginScreen";
 import Signup from "../Start/SignupScreen";
 import SpecialGames from "../Special Games/SpecialGame";
+import TripScreen from "../TripInfo/TripScreen";
 import AnyDayScreen from "../Schedule/AnyDayScreen";
 import InfoScreen from "../More/Info";
 import Help1Screen from "../Help/Help1Screen";
@@ -10,7 +12,6 @@ import Help2Screen from "../Help/Help2Screen";
 import BookNowScreen from "../Special Games/BookNow";
 
 
-import HeaderOptions from "./Header";
 
 const Stack = createStackNavigator();
 
@@ -21,41 +22,40 @@ const StartStackNavigator = ({ navigation }) => {
             <Stack.Screen name="Signup" navigationProps={navigation} component={Signup} />
         </Stack.Navigator>
     );
-};
+}
 
 const TripStackNavigator = ({ navigation }) => {
     return (
-        <Stack.Navigator screenOptions={{ headerStyle: { backgroundColor: '#F7F7F7' } }}>
-            <Stack.Screen name="book a trip" navigationProps={navigation} component={SpecialGames}
-                options={HeaderOptions({ navigation })} />
+        <Stack.Navigator>
+            <Stack.Screen name="book a trip" navigationProps={navigation} component={SpecialGames} />
+            <Stack.Screen name="book now" navigationProps={navigation} component={BookNowScreen} />
         </Stack.Navigator>
     );
-};
+}
 
 const MyBookingStackNavigator = ({ navigation }) => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="my bookings" navigationProps={navigation} component={AnyDayScreen}
-                options={HeaderOptions({ navigation })} />
+            <Stack.Screen name="my bookings" navigationProps={navigation} component={AnyDayScreen} />
         </Stack.Navigator>
     );
-};
+}
 
 const MoreStackNavigator = ({ navigation }) => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="more" navigationProps={navigation} component={InfoScreen} options={HeaderOptions({ navigation })} />
-            <Stack.Screen name="FAQ" navigationProps={navigation} component={Help1Screen} options={HeaderOptions({ navigation })} />
+            <Stack.Screen name="more" navigationProps={navigation} component={InfoScreen} />
+            <Stack.Screen name="FAQ" navigationProps={navigation} component={Help1Screen} />
         </Stack.Navigator>
     );
-};
+}
 
 const ContactStackNavigator = ({ navigation }) => {
     return (
         <Stack.Navigator>
-            <Stack.Screen name="contact us" navigationProps={navigation} component={Help2Screen} options={HeaderOptions({ navigation })} />
+            <Stack.Screen name="contact us" navigationProps={navigation} component={Help2Screen} />
         </Stack.Navigator>
     );
-};
+}
 
 export { StartStackNavigator, TripStackNavigator, MyBookingStackNavigator, MoreStackNavigator, ContactStackNavigator };
