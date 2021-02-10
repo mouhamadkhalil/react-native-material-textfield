2e7ab8a504d9e084aafe37e6a073e21b9b8f40f4
Karim Sabbagh <karims76@gmail.com>
2021-02-10 10:19:58 +0200

    add color

--- src/res/colors/index.js
+++ src/res/colors/index.js
@@ -2,7 +2,8 @@ const colors = {
     title: '#00B75D',
     text: '#0C222B',
     button: '#036675',
-    green: '#37CD0A'
-}
+    green: '#37CD0A',
+    greenLight: "#76FF02"
+};
 
-export default colors
+export default colors;
